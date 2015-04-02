4b3303bd3c0bc932492660e941b2fbfbe2873ea9
Oudin Samuel <essoversion@hotmail.com>
2015-04-01 21:12:26 -0400

    Update session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,11 +1,11 @@
 # Be sure to restart your server when you modify this file.
 
-#if Rails.env.production? && CatarseSettings.get_without_cache(:base_domain)
+if Rails.env.production? #&& CatarseSettings.get_without_cache(:base_domain)
   Catarse::Application.config.session_store :cookie_store, :key => '_sample_app_session' , :domain => '.pure-depths-2749.herokuapp.com'
   #Catarse::Application.config.session_store :cookie_store, key: CatarseSettings.get_without_cache(:secret_token), domain: CatarseSettings.get_without_cache(:base_domain)
-#else
-#  Catarse::Application.config.session_store :cookie_store, key: CatarseSettings.get_without_cache(:secret_token)
-#end
+else
+  Catarse::Application.config.session_store :cookie_store, key: CatarseSettings.get_without_cache(:secret_token)
+end
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
